10039c5ce0e0f866b85415d4d7d8e0f0c9f95c6f
AbdulrahmanIbraheem <abdulrahmanibraheem740@gmail.com>
2021-01-04 22:04:10 +0800

    assignent multiple variables to multiple values in one line

--- learn_type.py
+++ learn_type.py
@@ -10,3 +10,32 @@ print(num1)
 
 var = "Python Course"
 print(type(var))
+
+boolean = True
+boolean2 = False
+print(type( boolean) )
+print(type( boolean2) )
+print( "*" * 40 )
+# using multiple variables to multiple values in one line
+
+number1, number2, boolean1, boolean2, string = 23, 234.23, True, False, 'Omar'
+
+
+# print the values of number1, number2, boolean1, boolean2, string
+print(number1)
+print(number2)
+print(boolean1)
+print(boolean2)
+print(string)
+print()
+print("*" * 40)
+
+# print the types of number1, number2, boolean1, boolean2, string
+print()
+print(type( number1) )
+print(type( number2) )
+print(type( boolean1) )
+print(type( boolean2) )
+print(type( string) )
+print()
+print("*" * 40)
