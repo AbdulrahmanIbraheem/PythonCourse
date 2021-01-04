340a0d42d0aa25461f5827b36c8226c669eedfd9
AbdulrahmanIbraheem <abdulrahmanibraheem740@gmail.com>
2021-01-04 22:48:40 +0800

    used //, %, + and concoatanite two strings

--- Operators.py
+++ Operators.py
@@ -6,11 +6,19 @@
 
 
 num = 12 
+string1 = 'Hello'
+string2 = 'World!'
 
 print(num + 12) # print 12 + 12 => 24
 print(num - 5) # print 12 - 5 => 7
 print(num * 4) # print 12 * 4 => 48
-print(num / 2) # print 12 / 2 => 6 [ return floating alue ]
+print(num / 2) # print 12 / 2 => 6.0 [ return floating alue ]
+print(num // 3) # print 12 // 3 => 4 return int value
+print(num % 5) # print 12 % 5 =>
+print(num + 123 ) # print 12 + 123 => 135
+print(string1 + ' ' + string2 ) # used + to concoatanite tow string
+
+print()
 
 
 
