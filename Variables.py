d716274d9a5e4caf72c304bd40875a4fe1f77db7
AbdulrahmanIbraheem <abdulrahmanibraheem740@gmail.com>
2021-01-04 21:31:56 +0800

    print food and price

--- Variables.py
+++ Variables.py
@@ -10,5 +10,11 @@ print(num)
 num1 = 22.3
 print(num1)
 
+# this is a str varable 
 var = 'Hello, Python '.title()
 print(var)
+
+food = "apple"
+price = 1.5
+
+print('The Price For One ' + food + ' is ' + str( price ) + '$')
