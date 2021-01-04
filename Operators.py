5e250db0565c3a181240cdba01f133db0084b2d9
AbdulrahmanIbraheem <abdulrahmanibraheem740@gmail.com>
2021-01-05 01:05:42 +0800

    used assignment operators

--- Operators.py
+++ Operators.py
@@ -15,10 +15,32 @@ print(num * 4) # print 12 * 4 => 48
 print(num / 2) # print 12 / 2 => 6.0 [ return floating alue ]
 print(num // 3) # print 12 // 3 => 4 return int value
 print(num % 5) # print 12 % 5 =>
+
 print(num + 123 ) # print 12 + 123 => 135
 print(string1 + ' ' + string2 ) # used + to concoatanite tow string
 
 print()
+
+print('*' * 40)
+print()
+
+# assignment operators
+
+# == , =!, >, <, >=, <=,
+
+# return eiether True or False if the condition is true will return true other wase will return false
+
+print(5 == 5) # True
+print(5 != 5) # False
+print(6 > 5)  # True
+print(6 < 5)  # False
+print(6 >= 6)# True
+print(6 <= 6) # True
+
+print('*' * 40)
+
+
+
 
 
 
