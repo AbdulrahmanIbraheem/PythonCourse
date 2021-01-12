11725d4c12082f50c6a4ff7198621c1fed4c59b9
AbdulrahmanIbraheem <abdulrahmanibraheem740@gmail.com>
2021-01-12 19:56:10 +0800

    learning funtion

---
+++ function.py
@@ -0,0 +1,44 @@
+
+# this to learn functions
+# i dont we argumaents in this type
+
+def summ():
+  suum = 4 + 34
+  print(suum)
+summ()
+
+
+def summ(num1,num2):
+  print(num1 + num2)
+summ(3 , 5)
+
+def string(firstName, lastName):
+  print(firstName.title() + ' ' + lastName)
+string('abdulrahman', 'shaweesh')
+
+
+def fullName(firstName,lastName):
+  fuulName = firstName + ' ' + lastName
+  print(fuulName.title())
+fullName('abdulrahman','shaweesh')
+
+
+def mm(x,y):
+  print(x + y)
+mm(x = 4, y = -1)
+
+
+def mm(x = 0, y = 0):
+  print(x + y)
+mm(2)
+
+
+
+def mm(x , y = 1):
+  print(x + y)
+mm(2)
+
+
+
+
+
